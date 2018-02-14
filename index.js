98489d6631df20d24e75c308a7261906f8f13c62
youssefatti <youssefattia@gmail.com>
2018-02-14 22:48:30 +0100

    airbnb fixed mail validate

--- index.js
+++ index.js
@@ -10,15 +10,22 @@ var bodyParser = require('body-parser')
 mongoose.connect('mongodb://localhost:27017/airbnb');
 
 app.use(bodyParser.json())
+//"Content-Type": "application/json" 
 
+var mailgun = require("mailgun-js");
+var api_key = 'key-20f1eb398afded65a8f1e4341e400f4a';
+var DOMAIN = 'sandboxa142765faced4725b05ca70454741094.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+ 
 app.post('/api/user/sign_up', function (req, res) {
-    var username = req.body.username;
+    //console.log(req + " de req");
+    var username = req.body.username;//.toLowerCase();
     var email = req.body.email;
     var password = req.body.password;
     var biography = req.body.biography;
     var salt = uid(64);
     var hash = SHA256(password+salt).toString(encBase64);
-    var token = uid(10);
+    var token = uid(32);
     
     var user = {
         account: {
@@ -28,14 +35,15 @@ app.post('/api/user/sign_up', function (req, res) {
         email: email,
         token: token,
         hash: hash,
-        salt: salt  
+        salt: salt,
+        activated: false
         }
 
-console.log(user)
     var newUser = new Users(user);
         newUser.save(function(err, user) {
             if (err) {
-            console.log("something went wrong");
+                console.log("something went wrong");
+                res.send(err);
             } 
             else {
                 var userResponse = {
@@ -49,66 +57,84 @@ console.log(user)
                 res.json(userResponse)
             }
         })
+
+    var data = {
+        from: 'airbnb validation <me@samples.mailgun.org>',
+        to: user.email,
+        subject: 'Validez votre compte',
+        text: 'Testing some Mailgun awesomness!'+ ' ' + 'http://localhost:3000/api/user/validation/'+ user.token,
+        };
+        
+        mailgun.messages().send(data, function (error, body) {
+            // console.log("send")
+            // console.log(data.text)
+            // console.log(body);
+        });
 });   
 
+app.get("/api/user/validation/:token", function(req, res){
+    
+    var token = req.params.token;
+    Users.findOneAndUpdate({token : token}, { activated : true, token : uid(32)}, {new: true}, function(err, user){
+        console.log("affiche le user "+user)
+        var data = {
+            from: 'airbnb <me@samples.mailgun.org>',
+            to: user.email,
+            subject: 'Votre compte est validé',
+            text: 'Bravo compte valide',
+            };
+            
+            mailgun.messages().send(data, function (error, body) {
+                // console.log("send")
+                // console.log(data.text)
+                // console.log(body);
+            });
+        res.json(user);
+        // user.update({ activated : true}, {token : uid(32)}, function(err, userUpdate){
 
-app.post("/api/user/log_in", function(req, res){
-    var email = req.body.email;
-    var password = req.body.password;
-    console.log("email " + email + " password " + password)
-    Users.find({ email: email }, function(err, user){
-        if (err) {
-            console.log("something went wrong");
-        } 
-        else {
-            console.log(user)
-            var salt = user[0].salt;
-            var hash = SHA256(password+salt).toString(encBase64);
-     
-            if (hash == user[0].hash){
-
-                console.log("is authenticated")
-
-                var userResponse = {
-                    _id: user[0]._id,
-                    token: user[0].token,
-                    account: {
-                    username: user[0].account.username,
-                    biography: user[0].account.biography
-                    }
-                }
-                res.json(userResponse);
-            } 
-        }
+        //     console.log("user update "+userUpdate)
+        // })
+        
+        // user.set({
+        //     activated : true,
+        //     token : uid(32)
+        // })
+        // user.save(function(err, userUpdate){
+        //     console.log("user update "+userUpdate)
+        // })
     })
-})
+    
+});
 
 app.get("/api/user/:id", function(req, res){
     var id = req.params.id;
-
-    if (token != ''){
-        var token = req.headers.authorization;
-
+    var token = req.headers.authorization;
+    if (token !== undefined){
         token = token.replace("Bearer", "").trim();
-
-        Users.find({ token: token }, function(err, user){
+        Users.find({ token: token }, function(err, user){ 
             if (!err){
-                if(token == user[0].token){
-                    console.log("isAuthenticated");
-                    Users.findById({ _id : id}, function(err, user){
-                        var userResponse = {
-                            _id: user._id,
-                            account: {
-                            username: user.account.username,
-                            biography: user.account.biography
-                            }
+                if (0 === user.length){
+                    res.status(401).json({
+                        "error": {
+                          "code": 9473248,
+                          "message": "Invalid token"
                         }
-                        console.log(userResponse)
-                        res.json(userResponse);
                     })
                 }
                 else{
-                    console.log("Unauthorized")
+                   // if(token == user[0].token){
+                        console.log("isAuthenticated");
+                        Users.findById({ _id : id}, function(err, user){
+                            var userResponse = {
+                                _id: user._id,
+                                account: {
+                                username: user.account.username,
+                                biography: user.account.biography
+                                }
+                            }
+                            res.json(userResponse);
+                        })
+                   // }
                 }
             }
         })
@@ -119,27 +145,123 @@ app.get("/api/user/:id", function(req, res){
               "code": 48326,
               "message": "Invalid token"
             }
-          })
+        })
     }
-
 })
 
+// Connexion avec l'email ou le username
 
-function isAuthenticated(token){
-    Users.find({ token: token }, function(err, res){
-        if (!err){
-            if(token == user[0].token){
-                console.log("isAuthenticated");
-                return true;
+app.post("/api/user/log_in", function(req, res){
+    var input = req.body.input;
+    var password = req.body.password;
+
+    var itsEmail = input.search("@");
+
+    console.log(itsEmail+" test")
+
+    if (itsEmail !== -1){
+        console.log("its a email")
+        email = input;
+        Users.find({ email: email }, function(err, user){
+            if (err) {
+                console.log("something went wrong");
+            } 
+            else {
+                var salt = user[0].salt;
+                var hash = SHA256(password+salt).toString(encBase64);
+         
+                if (hash == user[0].hash){
+                    console.log("is authenticated")
+                    var userResponse = {
+                        _id: user[0]._id,
+                        token: user[0].token,
+                        account: {
+                        username: user[0].account.username,
+                        biography: user[0].account.biography
+                        }
+                    }
+                    res.json(userResponse);
+                }
+                else{
+                    res.json("Unauthorized");
+                }
             }
-            else{
-                console.log("Unauthorized")
-                return false;
+        })
+    }
+    else if (itsEmail === -1){
+        username = input;//.toLowerCase();
+        Users.find( { "account.username" : username}, function(err, user){
+            if (err) {
+                console.log("something went wrong");
+            } 
+            else {
+                var salt = user[0].salt;
+                var hash = SHA256(password+salt).toString(encBase64);
+         
+                if (hash == user[0].hash){
+                    console.log("is authenticated")
+                    var userResponse = {
+                        _id: user[0]._id,
+                        token: user[0].token,
+                        account: {
+                        username: user[0].account.username,
+                        biography: user[0].account.biography
+                        }
+                    }
+
+                    res.json(userResponse);
+                }
+                else{
+                    res.json("Unauthorized");
+                }
             }
-        }
-    })
-}
+        })
+    }
+    else{
+        res.json("Erreur dans le champs input");
+    }
+})
+
+app.get("*", function(req, res){
+    res.status(404).send("Erreur de connection")
+})
 
 app.listen(3000, function(){
       console.log("server is connected")
 });
+
+// connexion avec seulement l'email
+
+// app.post("/api/user/log_in", function(req, res){
+//     var email = req.body.email;
+//     var password = req.body.password;
+//     //console.log("email " + email + " password " + password)
+
+
+
+//     Users.find({ email: email }, function(err, user){
+//         if (err) {
+//             console.log("something went wrong");
+//         } 
+//         else {
+//             console.log(user)
+//             var salt = user[0].salt;
+//             var hash = SHA256(password+salt).toString(encBase64);
+     
+//             if (hash == user[0].hash){
+
+//                 console.log("is authenticated")
+
+//                 var userResponse = {
+//                     _id: user[0]._id,
+//                     token: user[0].token,
+//                     account: {
+//                     username: user[0].account.username,
+//                     biography: user[0].account.biography
+//                     }
+//                 }
+//                 res.json(userResponse);
+//             } 
+//         }
+//     })
+// })
--- models/user.js
+++ models/user.js
@@ -1,16 +1,28 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require("mongoose-unique-validator");
 
 var users = new mongoose.Schema(
     {
         "account": {
-           "username": String,
+           "username": { // Permet de faire la validation du username afin d'evité les doublons
+                    type: String,
+                    required: [true, "User name is required"],
+                    unique: true
+        },
            "biography": String
         },
-        "email": String,
+        "email": { // Permet de faire la validation de l'email afin d'evité les doublons
+            type: String,
+            required: [true, "User name is required"],
+            unique: true
+},
         "token": String,
         "hash": String,
-        "salt": String
+        "salt": String,
+        "activated": Boolean
     });
   
+// Permet l'affichage d'un meilleur message d'erreur
+users.plugin(uniqueValidator, { message: "{PATH} is already taken" });
   // 2) Definir le model - A faire qu'une fois
 module.exports  = mongoose.model("Users", users);
